53c17d03aeb1b72a7d7cd5b7436be563c127b413
Hack Reactor Student <student@hackreactor.com>
2017-08-11 16:07:52 -0500

    Justin's Project

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-$(document).ready(function(){
-  
+$(document).ready(function() {
+  var submit = $("#submit1");
+  var form = $("#form1");
+  $(submit).click(function() {
+    var text = $("#text").val();
+    var contentLength = $('ul#myList > li').length;
+    var content = "<li>" + text + "</li>";
+    $('ul#myList').append(content);
+  });
 });
